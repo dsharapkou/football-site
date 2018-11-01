5fb2939e08c1e8108e8fb7dafd9d03e94eb60da9
Dmitry Sharapkou <dsharapkou@klika-tech.com>
2018-11-01 12:18:22 +0300

    First commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>Kikiki hallo!</h1>
+    }
+}
+
+export default Header;
